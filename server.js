a989ec2e94b3a98a227126f40ef808886519122d
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:20:05 +0300

    create welcomeMessage date & inject

--- server.js
+++ server.js
@@ -7,26 +7,16 @@ app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public')); //let express use /public folder
 
 app.get('/', (req, res) => {
-  // res.send('<h1>Hello Express</h1>');
-  // res.send({
-  //   name: 'Jassem',
-  //   likes: [
-  //     'coding',
-  //     'reading'
-  //   ]
-  // });
-
   res.render('home.hbs', {
-    pageTitle: 'About page',
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
     currentYear: new Date().getFullYear(),
-    name: 'Jassem Al-Buloushi',
-    websiteName: "www.JassemAwesome.com"
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
-    pageTitle: 'About page',
+    pageTitle: 'About Page',
     currentYear: new Date().getFullYear()
   });
 });
