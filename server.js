85f53350775b0d6f3791b3c47656bced4ea496f2
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:20:05 +0300

    move use public dir

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials'); //Let hbs use partials
 app.set('view engine', 'hbs');
-app.use(express.static(__dirname + '/public')); //let express use /public folder
+
 
 app.use((req, res, next) => {
   var now = new Date().toString();
@@ -25,6 +25,8 @@ app.use((req, res, next) => {
 app.use((req, res, next) => {
   res.render('maintenace.hbs');
 });
+
+app.use(express.static(__dirname + '/public')); //let express use /public folder
 
 hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
