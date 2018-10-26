4d664caff18fa7a07c2ca582326750dd9637842c
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-26 22:20:05 +0300

    remove currentYear from data

--- server.js
+++ server.js
@@ -16,14 +16,12 @@ app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
     welcomeMessage: 'Welcome to my website',
-    currentYear: new Date().getFullYear(),
   });
 });
 
 app.get('/about', (req, res) => {
   res.render('about.hbs', {
     pageTitle: 'About Page',
-    currentYear: new Date().getFullYear()
   });
 });
 
