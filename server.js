42207e857f74cd89e3556a23ad7f57a3d5b58024
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-10-31 13:06:36 +0300

    add middleware to use public folder

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 var app = express();
 
+app.use(express.static(__dirname + '/public')); //let express use /public folder
+
 app.get('/', (req, res) => {
   // res.send('<h1>Hello Express</h1>');
   res.send({
