685e9acb6e297524949cf19e02b137df70da84e7
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-02 00:39:54 +0300

    add hbs helper screamIt

--- server.js
+++ server.js
@@ -12,6 +12,10 @@ hbs.registerHelper('getCurrentYear', () => {
   return new Date().getFullYear();
 });
 
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
 app.get('/', (req, res) => {
   res.render('home.hbs', {
     pageTitle: 'Home Page',
