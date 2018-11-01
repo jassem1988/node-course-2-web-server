cf9f978f447c6b6c8b47989f62fd760ca3e8e068
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-01 11:17:31 +0300

    config partails

--- server.js
+++ server.js
@@ -3,6 +3,8 @@ const express = require('express'),
 
 var app = express();
 
+
+hbs.registerPartials(__dirname + '/views/partials'); //Let hbs use partials
 app.set('view engine', 'hbs');
 app.use(express.static(__dirname + '/public')); //let express use /public folder
 
