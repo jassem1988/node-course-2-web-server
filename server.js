52efbb5b9319018aac581b03ae6e4348ffb8e355
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-03 00:18:14 +0300

    move projects route

--- server.js
+++ server.js
@@ -50,15 +50,16 @@ app.get('/about', (req, res) => {
   });
 });
 
-app.get('/bad', (req, res) => {
-  res.send({
-    errorMessage: 'Unable to handle request'
-  });
-});
-
 app.get('/projects', (req, res) => {
   res.render('projects.hbs', {
     pageTitle: "Projects Page"
+  });
+});
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to handle request'
   });
 });
 
