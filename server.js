64cc35694646a633b92bd481953ccad89a954600
Jassem Al-Buloushi <jassem1988@gmail.com>
2018-11-03 00:13:40 +0300

    fix projects link & inject pageTitle

--- server.js
+++ server.js
@@ -57,7 +57,9 @@ app.get('/bad', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects.hbs');
+  res.render('projects.hbs', {
+    pageTitle: "Projects Page"
+  });
 });
 
 
